0cdcc0766d8447859c91e15819f589e178ea7b7a
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-08-03 15:04:04 +0200

    added Player components, added a MainPage layout wrapper

---
+++ containers/components/utils.go
@@ -0,0 +1,51 @@
+package components
+
+import (
+	"fmt"
+)
+
+var cols = [8]string{"a", "b", "c", "d", "e", "f", "g", "h"}
+var rows = [8]int{8, 7, 6, 5, 4, 3, 2, 1}
+
+type Square struct {
+	Piece              Piece
+	Selected           bool
+	CoordinatePosition [2]int
+	Coordinates        [2]int
+	Color              string
+}
+
+type Piece struct {
+	Name       string
+	Image      string
+	Tile       string
+	IsWhite    bool
+	LegalMoves [][]int
+	MovesOnce  bool
+	Moved      bool
+	IsKing     bool
+	IsPawn     bool
+}
+
+type PlayerStruct struct {
+	Name   string
+	Image  string
+	Timer  string
+	Pieces string
+}
+
+func genCol(color string) string {
+	return "background-color: " + color
+}
+
+func getPosX(row, multiplier int) string {
+	return fmt.Sprintf("top: %vpx", row*multiplier)
+}
+
+func getPosY(row, multiplier int) string {
+	return fmt.Sprintf("left: %vpx", row*multiplier-15)
+}
+
+func getPiecePos(cord [2]int) string {
+	return fmt.Sprintf("bottom: %vpx; left: %vpx", cord[0], cord[1])
+}
