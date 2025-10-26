99731cb617316a1117c1a3bb03f8b3d96c8a16be
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-10-26 16:39:39 +0100

    Plans for the refactor

--- types.go
+++ types.go
@@ -7,9 +7,10 @@ import (
 )
 
 type appConfig struct {
-	database    *database.Queries
-	secret      string
-	users       map[uuid.UUID]User
+	database *database.Queries
+	secret   string
+	users    map[uuid.UUID]User
+	// TODO: this can probably be combined into one struct, instead of multiple, Matches and connections
 	Matches     map[string]Match
 	connections map[string]OnlineGame
 }
@@ -28,13 +29,16 @@ type OnlineGame struct {
 }
 
 type Match struct {
-	board                 map[string]components.Square
-	pieces                map[string]components.Piece
-	selectedPiece         components.Piece
-	coordinateMultiplier  int
-	isWhiteTurn           bool
-	isWhiteUnderCheck     bool
-	isBlackUnderCheck     bool
+	//TODO: let's make board and pieces into it's own struct, so we can attach functions to it
+	board  map[string]components.Square
+	pieces map[string]components.Piece
+	//TODO: from the top of my head there is a lot of stuff I do with selectedPiece, so maybe we can make this into it's own struct to attach methods? this one is for discussion
+	selectedPiece        components.Piece
+	coordinateMultiplier int
+	isWhiteTurn          bool
+	isWhiteUnderCheck    bool
+	isBlackUnderCheck    bool
+	//TODO: same here as for selected piece, maybe even for allMoves, takenPiecesBlack and takenPiecesWhite
 	tilesUnderAttack      []string
 	blackTimer            int
 	whiteTimer            int
