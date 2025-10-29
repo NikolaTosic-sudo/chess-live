e94234bd63fe7062e3302f9d804848a29e879f25
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-10-29 18:26:20 +0100

    Created helper functions around user

---
+++ user.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/NikolaTosic-sudo/chess-live/internal/auth"
+	"github.com/NikolaTosic-sudo/chess-live/internal/database"
+)
+
+func (cfg *appConfig) getUser(r *http.Request) (database.User, error) {
+	user := database.User{}
+
+	userC, err := r.Cookie("access_token")
+
+	if err != nil || userC.Value == "" {
+		return user, fmt.Errorf("invalid access token")
+	}
+
+	userId, err := auth.ValidateJWT(userC.Value, cfg.secret)
+
+	if err != nil {
+		return user, err
+	}
+
+	user, err = cfg.database.GetUserById(r.Context(), userId)
+
+	if err != nil {
+		logError("user not found in the database", err)
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (cfg *appConfig) removeCookie(name string) http.Cookie {
+	cookie := http.Cookie{
+		Name:     name,
+		Value:    "",
+		Path:     "/",
+		MaxAge:   -1,
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteLaxMode,
+	}
+
+	return cookie
+}
+
+func (cfg *appConfig) makeCookie(name, value, path string) http.Cookie {
+	cookie := http.Cookie{
+		Name:     name,
+		Value:    value,
+		Path:     path,
+		MaxAge:   604800,
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteLaxMode,
+	}
+
+	return cookie
+}
+
+func (cfg *appConfig) getMultiplier(r *http.Request) (int, error) {
+	mC, noMc := r.Cookie("multiplier")
+
+	multiplier := 0
+	if noMc != nil {
+		multiplier = 80
+	} else {
+		mcInt, err := strconv.Atoi(mC.Value)
+		if err != nil {
+			return multiplier, err
+		}
+		multiplier = mcInt
+	}
+
+	return multiplier, nil
+}
