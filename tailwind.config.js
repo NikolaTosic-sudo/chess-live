9b8d8fc38e6787d219210c74fd2b85c5aa4021b0
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-11-09 17:44:37 +0100

    Configured tailwind and added some extra classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  content: [
-    "./**/*.templ",
-    "./**/*.go"
-  ],
+  content: ["./**/*.templ", "./**/*.go"],
   theme: {
+    screens: {
+      xs: "300px",
+      sm: "500px",
+      md: "700px",
+      lg: "900px",
+      xl: "1200px",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
